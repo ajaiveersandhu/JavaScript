dff196737d855183617de1adf1ca6c09d5c4b95b
AjaiVeer Singh <ajaiveersandhu@gmail.com>
2019-10-22 01:41:08 -0400

    updated the test_score code with better functionality

--- Assignments/Assignment4/test_scores/test_scores.js
+++ Assignments/Assignment4/test_scores/test_scores.js
@@ -8,52 +8,56 @@ function get_Id(id) {
 function addScore() {
 	let name_input = get_Id("name").value;
 	let score_input = parseFloat(get_Id("score").value);
-	names.push(name_input);
-	scores.push(score_input);
-	console.log(names);
-	console.log(scores);
+	if (names.includes(name_input)) {
+		get_Id("name").insertAdjacentHTML("afterend", `<p style="display: inline; font-size: 0.75rem; margin: 0 1rem; color:blue;"> We already have the entry by same name : ${name_input}</p>`);
+	} else {
+		names.push(name_input);
+		scores.push(score_input);
+	}
+
+	get_Id("display_results").onclick = displayResults;
+	get_Id("display_scores").onclick = displayScores;
 }
 
 function displayResults() {
-	let value_Holder = 0;
+	let average = 0;
 	for (let x = 0; x < scores.length; x++) {
-		value_Holder = scores[x];
+		average += scores[x];
 	}
 	let name_of_student = names[scores.indexOf((Math.max(...scores)))];
 
 	let display_String = "<h2>Results</h2>";
-	display_String += "<p>Average Score = " + value_Holder + "</p>";
-	display_String += "<p>High score = " + name_of_student + " with a score of " + (Math.max(...scores)) + "</p>";
+	display_String += `<p>Average Score = ${average/scores.length} </p>`;
+	display_String += `<p> High score = ${name_of_student} with a score of ${(Math.max(...scores))}</p>`;
 
 	get_Id("results").innerHTML = display_String;
 }
 
 function displayScores() {
-	let display_String = "<thead>" +
-							"<tr>" +
-							"<th>" +
-							"Name" +
-							"</th>" +
-							"<th>" +
-							"Score" +
-							"</th>" +
-							"</tr>" +
-							"</thead>" +
-							"<tbody>";
+	let id_check = get_Id("score_heading");
 
-	for (let x = 0; x < names.length; x++) {
-		display_String += "<tr>" +
-							"<td>" +
-							names[x] +
-							"</td>" +
-							"<td>" +
-							scores[x] +
-							"</td>" +
-							"</tr>";
+	if (id_check == null) {
+		let score_h2 = document.createElement("h2");
+		score_h2.id = "score_heading";
+		score_h2.innerHTML = `<h2>Scores</h2>`;
+		get_Id("scores_table").before(score_h2);
 	}
 
-	display_String += "</tbody>";
+	let display_String = `<thead>
+								<tr style="text-align : left;">
+								<th>Name</th>
+								<th>Score</th>
+								</tr>
+						  </thead>
+						  <tbody>`;
 
+	for (let x = 0; x < names.length; x++) {
+		display_String += `<tr>
+							<td>${names[x]}</td>
+							<td>${scores[x]}</td>
+						   </tr>`;
+	}
+	display_String += "</tbody>";
 	get_Id("scores_table").innerHTML = display_String;
 }
 
@@ -62,13 +66,3 @@ window.onload = function () {
 	get_Id("display_results").onclick = displayResults;
 	get_Id("display_scores").onclick = displayScores;
 };
-
-
-
-
-
-
-
-
-
-//check
