1ae101ac00616e62964c4d61bcdca16b77ea4786
AjaiVeer Singh <ajaiveersandhu@gmail.com>
2019-10-23 19:46:52 -0400

    assignment 5 : stop watch done

--- Assignments/Assignment5/clock_stopwatch/clock.js
+++ Assignments/Assignment5/clock_stopwatch/clock.js
@@ -1,87 +1,88 @@
-"use strict";
-var $ = function(id) { return document.getElementById(id); };
+function time() {
+	let currentTime = new Date();
+	return currentTime;
+}
 
-//global stop watch timer variable and elapsed time object
-var stopwatchTimer;
-var elapsedMinutes = 0;
-var elapsedSeconds = 0;
-var elapsedMilliseconds = 0;
-
-var displayCurrentTime = function() {
-    var now = new Date();
-    var hours = now.getHours();
-    var ampm = "AM"; // set default value
-    
-    // correct hours and AM/PM value for display
-    if (hours > 12) { // convert from military time
-        hours = hours - 12;
-        ampm = "PM";
-    } else { // adjust 12 noon and 12 midnight
-         switch (hours) {
-            case 12: // noon
-                ampm = "PM";
-                break;
-            case 0:  // midnight
-                hours = 12;
-                ampm = "AM";
-        }
-    }
-    
-    $("hours").firstChild.nodeValue = hours;
-    $("minutes").firstChild.nodeValue = padSingleDigit(now.getMinutes());
-    $("seconds").firstChild.nodeValue = padSingleDigit(now.getSeconds());
-    $("ampm").firstChild.nodeValue = ampm;
+let displayCurrentTime = function () {
+	let currentHour = padSingleDigit(time().getHours());
+	let timeOfDay = "PM";
+	if (currentHour > 12) {
+		currentHour = currentHour - 12;
+		currentHour = padSingleDigit(currentHour);
+	} else {
+		current_hour = padSingleDigit(currentHour);
+		timeOfDay = "AM";
+	}
+	document.querySelector("#hours").innerHTML = currentHour;
+	document.querySelector("#minutes").innerHTML = padSingleDigit(time().getMinutes());
+	document.querySelector("#seconds").innerHTML = padSingleDigit(time().getSeconds());
+	document.querySelector("#ampm").innerHTML = timeOfDay;
 };
 
-var padSingleDigit = function(num) {
-	if (num < 10) {	return "0" + num; }
-	else { return num; }
-};
+function padSingleDigit(num) {
+	if (num < 10) {
+		return "0" + num;
+	} else {
+		return num;
+	}
+}
 
-var tickStopwatch = function() {    
-    // increment milliseconds by 10 milliseconds
-    
-    // if milliseconds total 1000, increment seconds by one and reset milliseconds to zero
-    
-    // if seconds total 60, increment minutes by one and reset seconds to zero
-    
-    //display new stopwatch time
-    
-};
+let tick = 0;
+let tickSeconds = 0;
+let tickMinutes = 0;
+let tickTimer = 0;
+
+function tickStopWatch() {
+	tick += 10;
+	document.querySelector("#s_ms").innerHTML = tick;
+
+	if (tick === 1000) {
+		tickSeconds += 1;
+		document.querySelector("#s_seconds").innerHTML = tickSeconds;
+		tick = 0;
+	}
+
+	if (tickMinutes === 60) {
+		tickMinutes += 1;
+		document.querySelector("#s_minutes").innerHTML = tickMinutes;
+		tickSeconds = 0;
+	}
+}
 
 // event handler functions
-var startStopwatch = function(evt) {
-    // prevent default action of link
-        
-    // do first tick of stop watch and then set interval timer to tick 
-    // stop watch every 10 milliseconds. Store timer object in stopwatchTimer 
-    // variable so next two functions can stop timer.
-    
+var startStopwatch = function (evt) {
+	evt.preventDefault();
+	tickTimer = setInterval(tickStopWatch, 10);
+	// do first tick of stop watch and then set interval timer to tick 
+	// stop watch every 10 milliseconds. Store timer object in stopwatchTimer 
+	// variable so next two functions can stop timer.
+
 };
 
-var stopStopwatch = function(evt) {
-    // prevent default action of link
-        
-    // stop timer
-    
+var stopStopwatch = function (evt) {
+	// prevent default action of link
+	evt.preventDefault();
+	clearTimeout(tickTimer);
+	// stop timer
+
 };
 
-var resetStopwatch = function(evt) {
-    // prevent default action of link
-        
-    // stop timer
-        
-    // reset elapsed variables and clear stopwatch display
-    
+var resetStopwatch = function (evt) {
+	// prevent default action of link
+	evt.preventDefault();
+	clearTimeout(tickTimer);
+	document.querySelector("#s_ms").innerHTML = 00;
+	document.querySelector("#s_seconds").innerHTML = 00;
+	document.querySelector("#s_minutes").innerHTML = 00;
+	// stop timer
+
+	// reset elapsed variables and clear stopwatch display
+
 };
 
-window.onload = function() {
-    // set initial clock display and then set interval timer to display
-    // new time every second. Don't store timer object because it 
-    // won't be needed - clock will just run.
-    displayCurrentTime();
-    setInterval(displayCurrentTime, 1000);
-    
-    // set up stopwatch event handlers
-
+window.onload = function () {
+	let clockTimer = setInterval(displayCurrentTime, 1000);
+	document.querySelector("#start").addEventListener("click", startStopwatch);
+	document.querySelector("#stop").addEventListener("click", stopStopwatch);
+	document.querySelector("#reset").addEventListener("click", resetStopwatch);
 };
