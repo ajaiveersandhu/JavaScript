e588169b1102d1f01d3cda3834a7e45051061de0
AjaiVeer Singh <ajaiveersandhu@gmail.com>
2019-10-30 15:26:29 -0400

    trying to implement autoplay in image gallery

--- Projects/ImageGallery/js/script.js
+++ Projects/ImageGallery/js/script.js
@@ -2,9 +2,24 @@ $(document).ready(function () {
 
     let $thumbnailClick = $(".thumbnails").find("a");
     let $displayImage = $(".imageView").find("img");
+    let pauseAtEachImage = 500;
+    let selectImage = 0;
+    let inteval;
+
+    let fetchSrc;
 
     $thumbnailClick.click(function (event) {
         event.preventDefault();
         $displayImage.attr("src", $(this).attr("href"));
     })
+
+    function autoplay() {
+        inteval = setInterval(function () {
+            // fetchSrc = $thumbnailClick[selectImage].f
+            $displayImage.attr("src", );
+        }, pauseAtEachImage);
+    }
+
+    // autoplay();
+
 });
