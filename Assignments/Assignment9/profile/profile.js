4127dc3faa31d7bb56dddc01ff637ae9d96c25bf
AjaiVeer Singh <ajaiveersandhu@gmail.com>
2019-11-21 02:57:39 -0500

    assignment 9 completed

--- Assignments/Assignment9/profile/profile.js
+++ Assignments/Assignment9/profile/profile.js
@@ -33,38 +33,54 @@ $(document).ready(function(){
         $("span").text("");   // clear any previous error messages
         var isValid = true;   // initialize isValid flag
         
-        var email = $("#email").val();
-        var phone = $("#phone").val();
-        var zip = $("#zip").val();
-        var dob = $("#dob").val();
+        // var email = $("#email").val();
+        // var phone = $("#phone").val();
+        // var zip = $("#zip").val();
+        // var dob = $("#dob").val();
+
+        // associative array
+        let associativeArray = [];
+
+        associativeArray["email"] = $("#email").val();
+        associativeArray["phone"] = $("#phone").val();
+        associativeArray["zip"] = $("#zip").val();
+        associativeArray["dob"] = $("#dob").val();
+
         
-        if ( email === "" || 
-                !email.match(/^[\w\.\-]+@[\w\.\-]+\.[a-zA-Z]+$/) ) 
+        if (associativeArray["email"] === "" ||
+                !associativeArray["email"].match(/^[\w\.\-]+@[\w\.\-]+\.[a-zA-Z]+$/))
         {
             isValid = false;
             $( "#email" ).next().text("Please enter a valid email.");
         }
-        if ( phone === "" || !phone.match(/^\d{3}-\d{3}-\d{4}$/) ) {
+        if ( associativeArray["phone"] === "" || !associativeArray["phone"].match(/^\d{3}-\d{3}-\d{4}$/) ) {
             isValid = false;
             $( "#phone" ).next().text(
                 "Please enter a phone number in NNN-NNN-NNNN format.");
         }
-        if ( zip === "" || !zip.match(/^\d{5}(-\d{4})?$/) ) {
+        if ( associativeArray["zip"] === "" || !associativeArray["zip"].match(/^\d{3}(-\d{3})?$/) ) {
             isValid = false;
             $( "#zip" ).next().text("Please enter a valid zip code.");
         }
-        if ( dob === "" || !isDate(dob) ) {
+        if ( associativeArray["dob"] === "" || !isDate(associativeArray["dob"]) ) {
             isValid = false;
             $( "#dob" ).next().text(
                 "Please enter a valid date in MM/DD/YYYY format.");
         }
         
         if ( isValid ) { 
-            // code that saves profile info goes here
-            sessionStorage.email = email;
-            sessionStorage.phone = phone;
-            sessionStorage.zip = zip;
-            sessionStorage.dob = dob;
+            // code for associativeArray to save in profile session storage
+            let profile = "";
+            console.log(associativeArray);
+            for(let x in associativeArray) {
+                profile += `${x}=${associativeArray[x]}|`;
+            }
+
+            sessionStorage.setItem("profile", profile);
+            // sessionStorage.email = email;
+            // sessionStorage.phone = phone;
+            // sessionStorage.zip = zip;
+            // sessionStorage.dob = dob;
             
             // go to profile page
             location.href = "profile.html";
