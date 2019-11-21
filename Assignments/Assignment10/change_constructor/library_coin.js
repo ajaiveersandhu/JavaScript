1ee14f474d84f920f72ff2a670cf70c214d15ad5
AjaiVeer Singh <ajaiveersandhu@gmail.com>
2019-11-21 17:51:12 -0500

    assignment 10 part 2 done

--- Assignments/Assignment10/change_constructor/library_coin.js
+++ Assignments/Assignment10/change_constructor/library_coin.js
@@ -1,2 +1,24 @@
 "use strict";
 
+class Coins {
+    constructor(cents) {
+        this.cents = cents;
+    }
+    
+    isValid() {
+        if(isNaN(parseInt(this.cents))) {
+            alert("Cents entry must be NUMERIC.");
+            if(this.cents < 0 || this.cents > 99) {
+                alert("Valid range is 0 to 99.");
+            }
+        }
+    }
+    
+    getNumber(divisor) {
+        let divisorFactor = Math.floor((this.cents) / divisor);
+        this.cents = this.cents - (divisor * divisorFactor);
+        return divisorFactor;
+//        console.log(divisorFactor);
+//        console.log(output);
+    }
+};
