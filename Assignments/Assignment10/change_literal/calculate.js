ecf7a2b45c3726b6b11ea0958c586b44e93ff52f
AjaiVeer Singh <ajaiveersandhu@gmail.com>
2019-11-21 17:40:07 -0500

    change literal part completed

--- Assignments/Assignment10/change_literal/calculate.js
+++ Assignments/Assignment10/change_literal/calculate.js
@@ -5,36 +5,52 @@ $(document).ready(function() {
     
         // get the number of cents from the user
         cents = Math.floor( parseInt( $("#cents").val() ) );
+        Coins.cents = cents;
+        Coins.isValid();
+        quarters = Coins.getNumber(25);
+        dimes = Coins.getNumber(10);
+        nickels = Coins.getNumber(5);
+        pennies = Coins.getNumber(1)
+        
+        
+        // display the results of the calculations
+        $("#quarters").val( quarters );
+        $("#dimes").val( dimes );
+        $("#nickels").val( nickels );
+        $("#pennies").val( pennies );
 
-        if (isNaN(cents) || cents < 0 || cents > 99) {
-            alert("Please enter a valid number between 0 and 99");
-        } else {
-            // calculate the number of quarters
-            quarters = cents / 25;      // get number of quarters
-            quarters = Math.floor(quarters);
-            cents = cents % 25;         // assign the remainder to the cents variable
+        // select cents text box for next entry
+        $("#cents").select();
 
-            // calculate the number of dimes
-            dimes = cents / 10;         // get number of dimes
-            dimes = Math.floor(dimes);
-            cents = cents % 10;         // assign the remainder to the cents variable
-
-            // calculate the number of nickels
-            nickels = cents / 5;
-            nickels = Math.floor(nickels);
-
-            // calculate the number of nickels and pennies
-            pennies = cents % 5;
-
-            // display the results of the calculations
-            $("#quarters").val( quarters );
-            $("#dimes").val( dimes );
-            $("#nickels").val( nickels );
-            $("#pennies").val( pennies );
-            
-            // select cents text box for next entry
-            $("#cents").select();
-        }
+//        if (isNaN(cents) || cents < 0 || cents > 99) {
+//            alert("Please enter a valid number between 0 and 99");
+//        } else {     
+//            // calculate the number of quarters
+//            quarters = cents / 25;      // get number of quarters
+//            quarters = Math.floor(quarters);
+//            cents = cents % 25;         // assign the remainder to the cents variable
+//
+//            // calculate the number of dimes
+//            dimes = cents / 10;         // get number of dimes
+//            dimes = Math.floor(dimes);
+//            cents = cents % 10;         // assign the remainder to the cents variable
+//
+//            // calculate the number of nickels
+//            nickels = cents / 5;
+//            nickels = Math.floor(nickels);
+//
+//            // calculate the number of nickels and pennies
+//            pennies = cents % 5;
+//
+//            // display the results of the calculations
+//            $("#quarters").val( quarters );
+//            $("#dimes").val( dimes );
+//            $("#nickels").val( nickels );
+//            $("#pennies").val( pennies );
+//            
+//            // select cents text box for next entry
+//            $("#cents").select();
+//        }
     }); // end click()
     
     // set focus on initial load
--- Assignments/Assignment10/change_literal/library_coin.js
+++ Assignments/Assignment10/change_literal/library_coin.js
@@ -1,2 +1,22 @@
 "use strict";
 
+let Coins = {
+    cents: 0,
+    
+    isValid: function() {
+        if(isNaN(parseInt(this.cents))) {
+            alert("Cents entry must be NUMERIC.");
+            if(this.cents < 0 || this.cents > 99) {
+                alert("Valid range is 0 to 99.");
+            }
+        }
+    },
+    
+    getNumber: function(divisor) {
+        let divisorFactor = Math.floor((this.cents) / divisor);
+        this.cents = this.cents - (divisor * divisorFactor);
+        return divisorFactor;
+//        console.log(divisorFactor);
+//        console.log(output);
+    }
+};
